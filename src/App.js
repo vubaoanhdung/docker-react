3a995010d78f901936d99473282eea2c910d1f6a
BAO VU <bav965@usask.ca>
2021-09-30 15:35:10 -0600

    change text app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Hi From Damon
                 </a>
             </header>
         </div>
